51433f61e0b4d73ab95d424d589b7f69ccd882d3
Ghassengaston0 <ghassengaston0@gmail.com>
2023-07-14 00:35:17 -0700

    g

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,5 +5,7 @@
  * Return: 0 indicate success.
  */
 int main(void)
-	{puts("\"Programming is like building a multilingual puzzle");
-	return (0);}
+	{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+	}
