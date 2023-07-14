1d278aaeed7b51e14d3eacc968a1fb0d5769d744
Ghassengaston0 <ghassengaston0@gmail.com>
2023-07-14 10:48:19 -0700

    g

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,27 +1,18 @@
 #include <stdio.h>
 
-/**
- * print_alphabet - Prints the alphabet in lowercase, followed by a space,
- *                  and then in uppercase, followed by a new line.
- */
-void print_alphabet(void)
-{
-	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+void print_alphabet() {
+    char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ";
 
-	for (int i = 0; i < 53; i++)
-		putchar(alphabet[i]);
+    // Print alphabet and space
+    for (int i = 0; i < 53; i++) {
+        putchar(alphabet[i]);
+    }
 
-	putchar('\n');
+    putchar('\n');
 }
 
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
+int main() {
+    print_alphabet();
+    return 0;
 }
 
