78bf355cc1f5af6857e71d6db73dc178aa2e35ee
Ghassengaston0 <ghassengaston0@gmail.com>
2023-07-14 17:21:27 -0700

    g

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,18 +6,14 @@
  * Return: 0 indicates success
  */
 int main(void)
-{
-    int a = 'a';
-    int A = 'A';
-
-    for (; a <= 'z'; a++)
-        putchar(a);
-
-    for (; A <= 'Z'; A++)
-        putchar(A);
-
-    putchar('\n');
-
-    return 0;
-}
+	{
+		int a = 'a';
+		int A = 'A';
+		for (; a <= 'z'; a++)
+			putchar(a);
+		for (; A <= 'Z'; A++)
+			putchar(A);
+		putchar('\n');
+		return (0);
+		}
 
