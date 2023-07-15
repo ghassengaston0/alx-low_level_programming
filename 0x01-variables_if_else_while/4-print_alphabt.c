8ff35e3df5635437db6268d666f346191e219dae
Ghassengaston0 <ghassengaston0@gmail.com>
2023-07-14 17:37:06 -0700

    g

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Return: 0 indicate success.
+ */
+int main(void)
+{
+int a = 'a';
+for (;a <= 'z'; a++)
+{
+if (a != 'e' && a != 'q')
+putchar (a);
+}
+putchar(\n');
+return (0);
+}
