ab8bf37cb7ddb9177bdfe0a640850ba6ada7000c
Ghassengaston0 <ghassengaston0@gmail.com>
2023-07-14 18:03:11 -0700

    g

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Return: 0 indicate success.
+ */
+int main(void)
+{
+int a, b, c;
+
+for (a = '0'; a <= '9'; a++)
+{
+for (b = a + 1 ; b <= '9' ; b++)
+{
+for (c = b + 1 ; c <= '9' ; c++)
+			{
+putchar (a);
+putchar (b);
+putchar (c);
+if (a == '7' && b == '8' && c == '9')
+break;
+putchar (',');
+putchar (' ');
+		}
+		}
+
+}
+putchar ('\n');
+return (0);
+}
