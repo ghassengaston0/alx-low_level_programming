213921c5875cf8d74f8c33dde7bdfe97a2d674b2
Ghassengaston0 <ghassengaston0@gmail.com>
2023-07-18 18:37:16 -0700

    g

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,9 @@
  */
 int print_last_digit(int n)
 {
-	{
-		int last = n % 10;
-		if (n < 0)
-			last *= -1;
-		_putchar (last + '0');
-		return (last);
-	}
+	int last = n % 10;
+	if (n < 0)
+		last *= -1;
+	_putchar (last + '0');
+	return (last);
+}
