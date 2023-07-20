7ccfd08f64ecc41f3dc9fdb16bb6aabaa611c276
Ghassengaston0 <ghassengaston0@gmail.com>
2023-07-20 10:20:47 -0700

    g

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9 followed by a new line.
+ */
+void print_numbers(void)
+{
+	char number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		_putchar(number);
+	}
+	_putchar('\n');
+}
