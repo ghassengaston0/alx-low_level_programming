b0498a5e5d296d9e874e94d7e449a614f5ff689b
Ghassengaston0 <ghassengaston0@gmail.com>
2023-07-20 16:06:48 -0700

    g

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,13 +7,16 @@
 
 void print_line(int n)
 {
-	int i;
-
-	if (n > 0)
-	for (i = 0 ; i < n ; i++)
-	{
-		_putchar ('_');
+	if (n <= 0)
+	{ 
+		_putchar('\n');
 	}
-	_putchar('\n');
-
+	else
+	{
+		for (int i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
