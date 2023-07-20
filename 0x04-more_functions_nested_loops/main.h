bd13c350f595450e0a55aca1cb1115df3ba33e1a
Ghassengaston0 <ghassengaston0@gmail.com>
2023-07-20 09:49:00 -0700

    g

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+void print_triangle(void);
+void print_square(void);
+void print_diagonal(void);
+void print_line(void);
+void more_numbers(void);
+void print_most_numbers(void);
+int _putchar(char c);
+void print_numbers(void);
+int mul(int a, int b);
+int _isdigit(int c);
+int _isupper(int c);
+
+#endif /* MAIN_H */
