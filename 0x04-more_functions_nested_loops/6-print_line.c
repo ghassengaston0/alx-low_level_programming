28f2ed59c2dfcb8e104593561b6e70190a1feea6
Ghassengaston0 <ghassengaston0@gmail.com>
2023-07-20 17:28:21 -0700

    g

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,7 +3,6 @@
 /**
  * print_line - draws a straight line in the terminal.
  * @n: number of times the character '_' should be printed
- * Return: void
  */
 
 void print_line(int n)
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square followed by a new line.
+ * @size:n  the size of the square
+ */
+void print_square(int n)
+	if (n  <= 0)
+{
+	_putchar('\n');
+	return;
+}
+else
+{
+	int i;
+	for (i = 0; i > n; i++)
+	{
+		_putchar('#')
+	}
+	_putchar('\n')
+}
+
+
