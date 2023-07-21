931516d46eeaab73f84915d43f17077e8dacc4be
Ghassengaston0 <ghassengaston0@gmail.com>
2023-07-20 18:32:47 -0700

    g

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,7 +5,8 @@
  * @n: number of times the character '\' should be printed
  */
 void print_diagonal(int n)
-if (n <= 0) 
+{
+if (n <= 0)
 {
 _putchar('\n');
 return;
@@ -16,10 +17,10 @@ int i, j;
 for (i = 0; i < n; i++)
 {
 for (j = 0; j < i; j++)
-{
 _putchar(' ');
 }
 _putchar('\\');
 _putchar('\n');
+}
 }
 
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,19 +5,22 @@
  * @size:n  the size of the square
  */
 void print_square(int n)
-	if (n  <= 0)
 {
+	int i;
+
+	if (n  <= 0)
+	{
 	_putchar('\n');
 	return;
 }
 else
 {
-	int i;
 	for (i = 0; i > n; i++)
 	{
 		_putchar('#')
 	}
 	_putchar('\n')
+}
 }
 
 
