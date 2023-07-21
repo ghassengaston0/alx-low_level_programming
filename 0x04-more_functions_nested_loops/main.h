10ff71eeb66a8d5346e4ce98520413a218c059b1
Ghassengaston0 <ghassengaston0@gmail.com>
2023-07-20 18:45:49 -0700

    g

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,17 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
-/* Function prototypes */
-void print_triangle(void);
-void print_square(void);
-void print_diagonal(void);
-void print_line(void);
-void more_numbers(void);
-void print_most_numbers(void);
 int _putchar(char c);
-void print_numbers(void);
-int mul(int a, int b);
-int _isdigit(int c);
 int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
-#endif /* MAIN_H */
+#endif
