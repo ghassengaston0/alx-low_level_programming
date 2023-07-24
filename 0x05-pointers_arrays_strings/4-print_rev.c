f8d71cc4706371d92d6c83252b6bd702e3ae4f59
Ghassengaston0 <ghassengaston0@gmail.com>
2023-07-23 21:26:26 -0700

    g

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,15 @@ void print_rev(char *s)
 		int o;
 
 		while (*s != '\0')
-			{
+		{
+
 				longi++;
 				s++;
+
 				}
 		s--;
 		for (o = longi; o > 0; o--)
-			{
+		{
 				_putchar(*s);
 				s--;
 				}
