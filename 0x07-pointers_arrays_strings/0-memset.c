6005b665983c215ea2c6fc50242a7733739b361e
Ghassengaston0 <ghassengaston0@gmail.com>
2023-08-01 03:41:04 +0200

    g

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,14 +9,12 @@ nclude "main.h"
  *        */
 char *_memset(char *s, char b, unsigned int n)
 {
-		int i = 0;
-
-			for (; n > 0; i++)
-					{
-								s[i] = b;
-										n--;
-											}
-				return (s);
+	int i = 0;
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
 }
-
 
