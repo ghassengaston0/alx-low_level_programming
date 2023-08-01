9d1307b3abf36a7e32c0e035d557f23ddadd737e
Ghassengaston0 <ghassengaston0@gmail.com>
2023-08-01 03:54:36 +0200

    g

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,41 +7,17 @@
  *      */
 char *_strpbrk(char *s, char *accept)
 {
-			int k;
+	int k;
 
-					while (*s)
-								{
-												for (k = 0; accept[k]; k++)
-																{
-																				if (*s == accept[k])
-																								return (s);
-																							}
-														s++;
-																}
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+			if (*s == accept[k])
+				return (s);
+		}
+		s++;
+	}
 
-						return ('\0');
+	return ('\0');
 }
-#include "main.h"
-/**
- *  * _strpbrk - Entry point
- *   * @s: input
- *    * @accept: input
- *     * Return: Always 0 (Success)
- *      */
-char *_strpbrk(char *s, char *accept)
-{
-			int k;
-
-					while (*s)
-								{
-												for (k = 0; accept[k]; k++)
-																{
-																				if (*s == accept[k])
-																								return (s);
-																							}
-														s++;
-																}
-
-						return ('\0');
-}
-
