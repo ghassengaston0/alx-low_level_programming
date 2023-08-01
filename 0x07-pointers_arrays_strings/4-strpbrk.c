aebeecba178292e073be5d26cbbde3ea0ca82849
Ghassengaston0 <ghassengaston0@gmail.com>
2023-08-01 04:25:44 +0200

    g

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,7 +8,6 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int k;
-
 	while (*s)
 	{
 		for (k = 0; accept[k]; k++)
@@ -18,6 +17,5 @@ char *_strpbrk(char *s, char *accept)
 		}
 		s++;
 	}
-
 	return ('\0');
 }
