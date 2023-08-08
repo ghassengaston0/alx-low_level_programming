2d1412c66cc9e83772fb57af7f071c3baececd8e
Ghassengaston0 <ghassengaston0@gmail.com>
2023-08-08 15:41:34 +0200

    g

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	me = malloc(sizeof(int*) * height);
+	me = malloc(sizeof(int *) * height);
 
 	if (me == NULL)
 		return (NULL);
