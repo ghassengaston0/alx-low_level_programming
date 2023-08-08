adffbec51e54851409501084b2853565e7585956
Ghassengaston0 <ghassengaston0@gmail.com>
2023-08-08 15:11:00 +0200

    g

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -29,15 +29,15 @@ char *str_concat(char *s1, char *s2)
 	i = ci = 0;
 	while (s1[i] != '\0')
 	{
-		conct[i] = s1[i];
+		concat[i] = s1[i];
 		i++;
 	}
 	while (s2[ci] != '\0')
 	{
-		conct[i] = s2[ci];
+		concat[i] = s2[ci];
 		i++, ci++;
 	}
-	conct[i] = '\0';
-	return (conct);
+	concat[i] = '\0';
+	return (concat);
 }
 
