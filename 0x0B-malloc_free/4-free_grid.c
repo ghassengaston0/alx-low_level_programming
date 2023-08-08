7f8c4538b020e5bc4836116e715553dde222e320
Ghassengaston0 <ghassengaston0@gmail.com>
2023-08-08 15:57:28 +0200

    g

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,10 +8,10 @@
  */
 void free_grid(int **grid, int height)
 {
-	if (grid == NULL || height <= 0)
-		return;
+	int i;
 
-	for (int i = 0; i < height; i++) {
+
+	for ( i = 0; i < height; i++) {
 		free(grid[i]);
 	}
 
